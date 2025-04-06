c1bf38530012491a64dc814c1761115644768706
Rishabh Mishra <rish3ha@gmail.com>
2025-04-06 17:11:20 +0530

    added google meta tag for verification

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+      <meta name="google-site-verification" content="Yyyc7ksU6zJ0U_ll-4OjzUZclWasAdxyEky6WBMUWD0" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
